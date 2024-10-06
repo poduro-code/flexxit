36d5a4c022544c3ed6041c66599de3279b7cf45c
poduro-code <oduro.prince383@gmail.com>
2024-10-07 00:27:58 +0100

    Adding temporary changes to Github

--- Documents/Flexxit/flexxit/pages/api/random.ts
+++ Documents/Flexxit/flexxit/pages/api/random.ts
@@ -3,8 +3,6 @@ import serverAuth from "@/lib/serverAuth";
 import prismadb from '@/lib/prismadb';
 
 
-
-
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     if (req.method !== 'GET') {
         return res.status(405).end(); 
